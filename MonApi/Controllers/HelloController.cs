86a923120a71af187c091ddf3d4d7948e2781f8f
MinixAda <77677992+MinixAda@users.noreply.github.com>
2026-03-06 15:36:57 +0100

    merdouille

--- MonApi/Controllers/HelloController.cs
+++ MonApi/Controllers/HelloController.cs
@@ -3,31 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 using MonApi.Services;
 
 
-
 namespace MonApi.Controllers
 {
 
     [ApiController]
-    [Route("api")]
+    [Route("api")]         
+
     public class HelloController : ControllerBase
     {
-
         private readonly MathService _mathService;
 
-   
         public HelloController(MathService mathService)
         {
             _mathService = mathService;
         }
 
-        [HttpGet("hello")]  // 
+        [HttpGet("hello")]   // Route finale : GET /api/hello
         public IActionResult Hello()
         {
-           
-            int resultat = _mathService.Addition(40, 2);
+            int resultat = _mathService.Addition(40, 2); // 40 + 2 = 42
 
-    
-            return Ok(new { message = $"Bonjour {resultat}" });
+            return Ok(new { message = $"Bonjour {resultat}" }); // {"Bonjour 42"}
         }
     }
 }
